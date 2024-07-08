a995a12e7bfc22c685af2e7b2973cab687fec02e
Lam Chau <lam@squareup.com>
2024-07-07 17:51:29 -0700

    chore: add timestamp to history

--- functions/__fzf_reverse_isearch.fish
+++ functions/__fzf_reverse_isearch.fish
@@ -1,6 +1,9 @@
 function __fzf_reverse_isearch
     history merge
-    history -z | eval (__fzfcmd) --read0 --print0 --tiebreak=index --toggle-sort=ctrl-r $FZF_DEFAULT_OPTS $FZF_REVERSE_ISEARCH_OPTS -q '(commandline)' | read -lz result
+    history -z --show-time="%F %H:%M:%S │ " \
+        | eval (__fzfcmd) --read0 --print0 --tiebreak=index --toggle-sort=ctrl-r $FZF_DEFAULT_OPTS $FZF_REVERSE_ISEARCH_OPTS -q '(commandline)' \
+        | cut -d'│' -f2- \
+        | read -lz result
     and commandline -- $result
     commandline -f repaint
 end
