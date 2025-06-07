2c4a9f559dc449b755d269219b16ef76b51ddf22
Lam Chau <lamchau@users.noreply.github.com>
2025-06-06 20:16:41 -0700

    refactor: modernize for fish 4.0+

--- functions/__fzf_cd.fish
+++ functions/__fzf_cd.fish
@@ -1,49 +1,32 @@
-function __fzf_cd -d "Change directory"
-    set -l commandline (__fzf_parse_commandline)
-    set -l dir $commandline[1]
-    set -l fzf_query $commandline[2]
+function __fzf_cd --description "Change directory"
+    set --local commandline (__fzf_parse_commandline)
+    set --local dir $commandline[1]
+    set --local fzf_query $commandline[2]
 
-    if not type -q argparse
-        # Fallback for fish shell version < 2.7
-        function argparse
-            functions -e argparse # deletes itself
-        end
-        if contains -- --hidden $argv; or contains -- -h $argv
-            set _flag_hidden "yes"
-        end
-    end
+    argparse h/hidden -- $argv
 
-    # Fish shell version >= v2.7, use argparse
-    set -l options  "h/hidden"
-    argparse $options -- $argv
+    set --local FZF_CD_COMMAND "
+        command find -L \"$dir\" -mindepth 1 \\( -path \"$dir\"'*/.*' -o -fstype sysfs -o -fstype devfs -o -fstype devtmpfs \\) -prune \
+        -o -type d -print 2> /dev/null | sed 's@^\./@@'"
 
-    set -l COMMAND
+    set --local FZF_CD_WITH_HIDDEN_COMMAND "
+        command find -L \"$dir\" \
+        \\( -path '*/.git*' -o -fstype dev -o -fstype proc \\) -prune \
+        -o -type d -print 2> /dev/null | sed 1d | cut -b3-"
 
-    set -q FZF_CD_COMMAND
-    or set -l FZF_CD_COMMAND "
-    command find -L \$dir -mindepth 1 \\( -path \$dir'*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
-    -o -type d -print 2> /dev/null | sed 's@^\./@@'"
-
-    set -q FZF_CD_WITH_HIDDEN_COMMAND
-    or set -l FZF_CD_WITH_HIDDEN_COMMAND "
-    command find -L \$dir \
-    \\( -path '*/\\.git*' -o -fstype 'dev' -o -fstype 'proc' \\) -prune \
-    -o -type d -print 2> /dev/null | sed 1d | cut -b3-"
-
-    if set -q _flag_hidden
-        set COMMAND $FZF_CD_WITH_HIDDEN_COMMAND
+    set --local find_command
+    if set --query _flag_hidden
+        set find_command $FZF_CD_WITH_HIDDEN_COMMAND
     else
-        set COMMAND $FZF_CD_COMMAND
+        set find_command $FZF_CD_COMMAND
     end
 
-    eval "$COMMAND | "(__fzfcmd)" +m $FZF_DEFAULT_OPTS $FZF_CD_OPTS --query \"$fzf_query\"" | read -l select
+    eval "$find_command | "(__fzfcmd)" +m $FZF_DEFAULT_OPTS $FZF_CD_OPTS --query \"$fzf_query\"" | read --local select
 
-    if not test -z "$select"
+    if test -n "$select"
         builtin cd "$select"
-
-        # Remove last token from commandline.
-        commandline -t ""
+        commandline --replace ""
     end
 
-    commandline -f repaint
+    commandline --function repaint
 end
