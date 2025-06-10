8c59f791603086787ce0f1206a18a958691f8546
Lam Chau <lamchau@gmail.com>
2025-06-09 18:14:21 -0700

    style: multiline options

--- functions/__fzf_reverse_isearch.fish
+++ functions/__fzf_reverse_isearch.fish
@@ -1,7 +1,14 @@
 function __fzf_reverse_isearch
     history merge
     history -z --show-time="%F %H:%M:%S │ " \
-        | eval (__fzfcmd) --read0 --print0 --tiebreak=index --toggle-sort=ctrl-r $FZF_DEFAULT_OPTS $FZF_REVERSE_ISEARCH_OPTS -q '(commandline)' \
+        | eval (__fzfcmd) \
+        --read0 \
+        --print0 \
+        --tiebreak=index \
+        --toggle-sort=ctrl-r \
+        $FZF_DEFAULT_OPTS \
+        $FZF_REVERSE_ISEARCH_OPTS \
+        -q '(commandline)' \
         | cut -d'│' -f2- \
         | read -lz result
     and commandline -- $result
