15aecae4a0f65a43b62cd3d7a37de565b28d9bcc
Lam Chau <lamchau@users.noreply.github.com>
2026-02-21 12:48:20 -0800

    refactor(fzfcmd): add __fzf_defaults helper, modernize __fzfcmd
    - add __fzf_defaults from fzf 0.68 pattern for consistent opts construction
      (--height, --min-height, --bind=ctrl-z:ignore, FZF_DEFAULT_OPTS_FILE support)
    - add FZF_TMUX_OPTS support to __fzfcmd (fzf 0.68)
    - use --long-options throughout

--- functions/__fzfcmd.fish
+++ functions/__fzfcmd.fish
@@ -1,9 +1,20 @@
-function __fzfcmd
-    set -q FZF_TMUX; or set FZF_TMUX 0
-    set -q FZF_TMUX_HEIGHT; or set FZF_TMUX_HEIGHT 40%
-    if test $FZF_TMUX -eq 1
-        echo "fzf-tmux -d$FZF_TMUX_HEIGHT"
+function __fzf_defaults --description "Build fzf options with defaults, opts file, and user overrides"
+    # Usage: __fzf_defaults "prepend opts" "append opts"
+    # Result: --height 40% --min-height=20+ --bind=ctrl-z:ignore $prepend $FZF_DEFAULT_OPTS_FILE $FZF_DEFAULT_OPTS $append
+    test -n "$FZF_TMUX_HEIGHT"; or set --local FZF_TMUX_HEIGHT 40%
+    string join ' ' -- \
+        "--height $FZF_TMUX_HEIGHT --min-height=20+ --bind=ctrl-z:ignore" $argv[1] \
+        (test -r "$FZF_DEFAULT_OPTS_FILE"; and string join -- ' ' <$FZF_DEFAULT_OPTS_FILE) \
+        $FZF_DEFAULT_OPTS $argv[2..-1]
+end
+
+function __fzfcmd --description "Return the fzf command, with tmux wrapper if configured"
+    test -n "$FZF_TMUX_HEIGHT"; or set --local FZF_TMUX_HEIGHT 40%
+    if test -n "$FZF_TMUX_OPTS"
+        echo "fzf-tmux $FZF_TMUX_OPTS -- "
+    else if test "$FZF_TMUX" = 1
+        echo "fzf-tmux -d$FZF_TMUX_HEIGHT -- "
     else
-        echo "fzf"
+        echo fzf
     end
 end
